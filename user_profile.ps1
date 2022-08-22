35339664b37bd29f9db9677e4eec249f55ca6236
MrShorcut <rmoreno@sier.com.co>
2022-08-22 08:28:39 -0500

    [ sort, add vimOpenFileAlias ]

--- user_profile.ps1
+++ user_profile.ps1
@@ -23,29 +23,22 @@ $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 # Alias
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
-Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-Set-Alias co code
-Set-Alias e explorer 
 Set-Alias .. cd..
 Set-Alias vi nvim
-Set-Alias gs gist
-Set-Alias ga. gitaddDot
 
-Set-Alias -Name gco -Value gcme 
 Set-Alias -Name ex -Value exitFn 
-Set-Alias -Name gll -Value gitll 
 Set-Alias -Name flush -Value flushdns
 Set-Alias -Name touch -Value touchFn
-Set-Alias -Name gin -Value gitInit 
 Set-Alias -Name v -Value nvimFN 
 Set-Alias -Name c -Value cdFN 
-Set-Alias -Name gpof -Value gpofFN 
-Set-Alias -Name gpob -Value gpobFN 
-Set-Alias -Name grsurl -Value grsurlFN
 Set-Alias -Name dev -Value devFN
+Set-Alias -Name e -Value eFN
+Set-Alias -Name n -Value nFN
+Set-Alias -Name ta -Value tFN
+Set-Alias -Name co -Value coFN
 
 # Utilities
 function which ($command) {
@@ -53,20 +46,8 @@ function which ($command) {
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-function grsurlFN ($param) {
-  git remote set-url origin $param
-}
-
-function gist {
-  git status
-}
-
 function nvimFN {
   nvim .
-}
-
-function gitInit {
-  git init
 }
 
 function flushdns {
@@ -82,6 +63,46 @@ function cdFN ($v) {
   ls
 }
 
+function touchFn ($value) {
+  new-Item -Name $value
+}
+
+function devFn {
+  npm run dev
+}
+
+function exitFN {
+  exit
+}
+
+function eFN {
+  explorer .
+}
+
+function coFN {
+  code .
+}
+
+function nFN ($p, $n, $i) {
+  # url, name, ico
+  nativefier $p -n $n -i $i
+}
+
+function tFN {
+  Write-Output '<script src="https://cdn.tailwindcss.com"></script>'
+}
+
+# Git
+Set-Alias g git
+Set-Alias gs gist
+Set-Alias ga. gitaddDot
+Set-Alias -Name grsurl -Value grsurlFN
+Set-Alias -Name gpof -Value gpofFN 
+Set-Alias -Name gco -Value gcme 
+Set-Alias -Name ginit -Value gitInit 
+Set-Alias -Name gll -Value gitll 
+Set-Alias -Name gpob -Value gpobFN 
+
 function gitaddDot ($p) {
   git add .
 }
@@ -95,22 +116,61 @@ function gpofFN {
 }
 
 function gpobFN($p) {
-  gitp push --set-upstream origin $p
+  git push --set-upstream origin $p
 }
 
-function touchFn ($value) {
-  new-Item -Name $value
+function grsurlFN ($param) {
+  git remote set-url origin $param
 }
 
-
-function devFn {
-  npm run dev
+function gist {
+  git status
+}
+function gitInit {
+  git init
 }
 
-function exitFN {
-  exit
+# Open files
+Set-Alias -Name vimp -Value vimPackageFN
+Set-Alias -Name vimr -Value vimReadmeFN
+Set-Alias -Name vimg -Value vimIgnoreFN
+Set-Alias -Name vimi -Value vimIndexFN
+Set-Alias -Name vimw -Value vimWebpackFN
+Set-Alias -Name vime -Value vimEditorconfigFN
+Set-Alias -Name vimt -Value vimTsconfigFN
+Set-Alias -Name vimu -Value vimUserProfileFN
+
+function vimPackageFN {
+  nvim package.json 
 }
 
+function vimReadmeFN {
+  nvim README.md
+}
+
+function vimIgnoreFN {
+  nvim .gitignore
+}
+
+function vimIndexFN {
+  nvim index.js
+}
+
+function vimWebpackFN {
+  nvim webpack.config.js
+}
+
+function vimEditorconfigFN {
+  nvim .editorconfig
+}
+
+function vimTsconfigFN {
+  nvim tsconfig.js
+}
+
+function vimUserProfileFN {
+  nvim user_profile.ps1
+}
 
 # Create .gitignore file using this command:
 # welc > .gitignore
